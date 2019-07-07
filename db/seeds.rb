0fa4a12d4ee2c315e71364e4975d982fb126f0b2
Matthew Hazlehurst <43639290+matthewhazlehurst@users.noreply.github.com>
2019-07-07 18:09:24 +0900

    Remove credentials from seeds.db and require ENV variables instead (#31)
    * Remove credentials from seeds.db and require ENV variables instead

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,15 +1,14 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+if ENV['ADMIN_EMAIL'] && ENV['ADMIN_PASSWORD']
 
-User.create!(name: "Matthew Hazlehurst",
-             email: "math.hazlehurst@gmail.com",
-             password: "password",
-             password: "password",
-             admin: true,
-             activated: true,
-             private: false)
+  User.create!(name: "Administrator",
+               email: ENV['ADMIN_EMAIL'],
+               password: ENV['ADMIN_PASSWORD'],
+               password_confirmation: ENV['ADMIN_PASSWORD'],
+               admin: true,
+               activated: true,
+               private: true)
+else
+
+  Rails.logger.debug "ADMIN_EMAIL and/or ADMIN_PASSWORD not specified for seeds data"
+
+end
